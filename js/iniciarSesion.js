0644814d2c5a4775149432083cfa2f6f53069f36
Gabriel Mariani <gabymariani20.11@gmail.com>
2024-11-28 17:17:55 -0300

    Se corrigio el color del formulario a uno mas 'aceptable', se aplico validación a los campos y se cambiaron algunos 'IDs' acorde a los nuevos protocolos de trabajo

--- js/iniciarSesion.js
+++ js/iniciarSesion.js
@@ -1,18 +1,45 @@
 $().ready(function () {
+    const $b_submit = $('#b_login');
+    
     $('#form_login').validate({
-        rules: { //toman
-            mail: "required",
-            password: "required"
+        rules: { 
+            i_login_mail: {
+                required: true,
+                email: true
+            },
+            i_login_password: "required"
         },
         messages: {
-            mail: {
-                required: "Este campo es obligatorio",
-                email: "El formato del email es incorrecto"
+            i_login_mail: {
+                required: "Debes ingresar tu correo",
+                email: "El formato del correo no es correcto"
             },
-            password:
-                "Este campo es obligatorio"
+            i_login_password:
+                "La contraseña es obligatoria"
+        },
+        errorElement: "div",
+        errorPlacement: function (error, element) {
+            error.addClass("invalid-feedback");
+            element.parent().append(error);
+        },
+        highlight: function (element) {
+            $(element).addClass("is-invalid").removeClass("is-valid");
+        },
+        unhighlight: function (element) {
+            $(element).addClass("is-valid").removeClass("is-invalid");
+        },
+
+        invalidHandler: function () {
+            $('<div class="alert alert-danger mt-3">Por favor, si no ingresa sus datos correctamente, no podra entrar.</div>')
+                .insertAfter($b_submit)
+                .fadeOut(5000, function () {
+                    $(this).remove();
+                });
         }
     });
 
+    $('#form_login input').on('blur', function(){
+        $(this).valid();
+    });
 
 });
