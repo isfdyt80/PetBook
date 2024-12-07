22ab68050a7a3ec26da7a80f76f5f2048a3761b9
Syrax <johanna.pallero@casares.gob.ar>
2024-12-07 18:11:25 -0300

    Habia 2 index, uno .php y otro .html, se dejó unicamente el .php y el archivo style.css que estaba en el raiz, la renombré por index.css y la metí dentro de la carpeta css

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <!-- conexion -->
-<?php 
-    include 'modales/m-nueva-publicacion.html';
-    include 'modales/m-perdido.html';
-    include 'modales/m-encontrado.html';
-    include 'modales/m-adopcion.html';
-    
+<?php
+include 'modales/m-nueva-publicacion.html';
+include 'modales/m-perdido.html';
+include 'modales/m-encontrado.html';
+include 'modales/m-adopcion.html';
+
 ?>
 
 <!DOCTYPE html>
@@ -15,9 +15,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PetBook</title>
     <link rel="stylesheet" href="/bootstrap/css/bootstrap.min.css">
-    <link rel="stylesheet" href="/css/index.css">
+    <link rel="stylesheet" href="css/index.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
-    
+
 </head>
 
 <body>
