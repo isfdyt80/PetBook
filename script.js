a5195f01bdec9dd655318f835989fa7ddb5b6562
JulianBarreiro <julian.barreiro_07@hotmail.com>
2025-02-24 17:17:44 -0300

    en esta rama hice los comentarios de la parte de scripts.js

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
+// Espera a que todo el contenido del DOM esté completamente cargado antes de ejecutar el script
 document.addEventListener("DOMContentLoaded", () => {
-    console.log("Página cargada correctamente");
+    console.log("Página cargada correctamente"); // Mensaje en la consola indicando que la página se ha cargado correctamente
 
+    // Selecciona el botón flotante en la página
     const floatingButton = document.querySelector(".floating-button");
 
+    // Agrega un evento de clic al botón flotante
     floatingButton.addEventListener("click", () => {
-        alert("¡Vamos a crear una nueva publicación!");
+        alert("¡Vamos a crear una nueva publicación!"); // Muestra una alerta al hacer clic en el botón
     });
 });
