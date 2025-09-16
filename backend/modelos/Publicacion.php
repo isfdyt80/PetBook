2eb33d2ae239f2518da3306e9ee7d48e9a9eed96
Gonzalez Juan M. <martinjuan2000spaik@gmail.com>
2025-09-16 20:59:22 -0300

    Se les agrego el contenido a los modelos que se subieron incorrectamente

--- backend/modelos/Publicacion.php
+++ backend/modelos/Publicacion.php
@@ -12,4 +12,17 @@ class Publicacion{
     public $ubicacion;  
     public $mascota_id;
     public $usuario_id;
+
+    public function __construct($descripcion, $estado, $mascota_id, $usuario_id, $publicacion_id = null, $activo = 1, $fecha_creacion = null, $foto = null, $recompensa = null, $ubicacion = null) {
+        $this->publicacion_id = $publicacion_id;
+        $this->activo    = $activo;
+        $this->descripcion = $descripcion;
+        $this->estado    = $estado;
+        $this->fecha_creacion = $fecha_creacion;
+        $this->foto      = $foto;
+        $this->recompensa = $recompensa;
+        $this->ubicacion = $ubicacion;
+        $this->mascota_id  = $mascota_id;
+        $this->usuario_id = $usuario_id;
+    }
 }
